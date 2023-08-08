d9c808d23c3a89b065e4e8b87122f79f2ce2e635
karimsemaan <semaan.k@northeastern.edu>
2023-08-08 09:09:53 +0300

    change

--- controllers/tuits/tuits-controller.js
+++ controllers/tuits/tuits-controller.js
@@ -2,6 +2,12 @@ import posts from "./tuits.js";
 let tuits = posts;
 
 const TuitsController = (app) => {
+  app.post('/api/tuits', createTuit);
+  app.get('/api/tuits', findTuits);
+  app.put('/api/tuits/:tid', updateTuit);
+  app.delete('/api/tuits/:tid', deleteTuit);
+};
+
     const createTuit = (req, res) => {
         const newTuit = req.body;
         newTuit._id = (new Date()).getTime() + '';
@@ -9,7 +15,7 @@ const TuitsController = (app) => {
         newTuit.liked = false;
         tuits.push(newTuit);
         res.json(newTuit);
-    }
+    };
     const findTuits = (req, res) =>
         res.json(tuits);
         const updateTuit = (req, res) => {
@@ -18,16 +24,12 @@ const TuitsController = (app) => {
             const tuitIndex = tuits.findIndex((t) => t._id === tuitdId)
             tuits[tuitIndex] = {...tuits[tuitIndex], ...updates};
             res.sendStatus(200);
-          }          
+          };        
     const deleteTuit = (req, res) => {
         const tuitdIdToDelete = req.params.tid;
         tuits = tuits.filter((t) =>
           t._id !== tuitdIdToDelete);
         res.sendStatus(200);
-      }      
-    app.post('/api/tuits', createTuit);
-    app.get('/api/tuits', findTuits);
-    app.put('/api/tuits/:tid', updateTuit);
-    app.delete('/api/tuits/:tid', deleteTuit);
-}
+      };     
+
 export default TuitsController;
